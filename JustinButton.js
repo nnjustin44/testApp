53222e1a5565828ff3b291927856f03906f53d67
nnjustin44 <nnjustin44@gmail.com>
2018-11-20 18:55:05 -0500

    changes button text

--- JustinButton.js
+++ JustinButton.js
@@ -7,7 +7,7 @@ export default class JustinButton extends React.Component{
         return(
             <View>
                 <Button >
-                    <Text>{this.props.buttonText} thot</Text>
+                    <Text>{this.props.buttonText} yo mamma</Text>
                 </Button>
             </View>
         )
